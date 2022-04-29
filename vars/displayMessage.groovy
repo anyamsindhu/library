f02b58fdbaa51b6f6522f373633412039b46d360
anyamsindhu <annemsindhu429@gmail.com>
2022-04-29 13:15:41 -0400

    added python file

--- vars/displayMessage.groovy
+++ vars/displayMessage.groovy
@@ -1,4 +1,17 @@
+class DisplayMessage implements CommandInterface{
+    def call(String user = 'test user') {
+        echo "hello ${user}"
+    }
+    def steps
+    void setSteps(def steps){
+        this.steps=steps
+    }
+    void execute()
+    {
 
-def call(String user='test user'){
-    echo "hello ${user}"
+      steps.sh 'echo "in library"'
+        steps.libraryResource('../resources/main.py')
+        steps.findFiles('main.py')
+    }
+
 }
